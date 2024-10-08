d571a78d15a012b6837f66f9a0b5936fbc5a1f87
sudo-Harshk <harshk1744@gmail.com>
2024-10-08 11:02:21 +0530

    modified:   index.js 	modified:   routes/subjects.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const fastify = require('fastify')({ logger: true });
-const helmet = require('@fastify/helmet');  // Correct import for helmet
-const cors = require('@fastify/cors');      // Correct import for CORS
+const helmet = require('@fastify/helmet');  
+const cors = require('@fastify/cors');     
 const subjectsRoutes = require('./routes/subjects');
 const questionsRoutes = require('./routes/questions');
 
 
-// Register plugins
-fastify.register(helmet); // Using the updated plugin
-fastify.register(cors);   // Using the updated CORS plugin
+fastify.register(helmet); 
+fastify.register(cors);   
 
-// Register routes
+
 fastify.register(subjectsRoutes);
 fastify.register(questionsRoutes);
 
-// Start server
+
 const start = async () => {
     try {
         await fastify.listen({ port: 3000 });
--- routes/subjects.js
+++ routes/subjects.js
@@ -1,12 +1,11 @@
 const subjectsData = require('../data/subjects');
 
 async function routes(fastify, options) {
-    // Route to get all subjects
     fastify.get('/subjects', async (request, reply) => {
         return Object.keys(subjectsData);
     });
 
-    // Route to get chapters for a specific subject
+    
     fastify.get('/subjects/:subject/chapters', async (request, reply) => {
         const { subject } = request.params;
         const chapters = subjectsData[subject];

b6d723b9cb3381087f3d41086cc3ce00f659b4ff
sudo-Harshk <harshk1744@gmail.com>
2024-10-08 12:24:23 +0530

    modified:   index.js 	modified:   package.json 	new file:   vercel.json

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 const fastify = require('fastify')({ logger: true });
 const helmet = require('@fastify/helmet');  
 const cors = require('@fastify/cors');     
-const subjectsRoutes = require('./routes/subjects');
-const questionsRoutes = require('./routes/questions');
-
+const subjectsRoutes = require('../routes/subjects');
+const questionsRoutes = require('../routes/questions');
 
 fastify.register(helmet); 
 fastify.register(cors);   
-
-
 fastify.register(subjectsRoutes);
 fastify.register(questionsRoutes);
 
-
-const start = async () => {
-    try {
-        await fastify.listen({ port: 3000 });
-        fastify.log.info(`Server listening on http://localhost:3000`);
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
+module.exports = async (req, res) => {
+  await fastify.ready();
+  fastify.server.emit('request', req, res);
 };
-
-start();

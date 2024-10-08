974c7541cea812b4f171e9f353fb84db9559752a
sudo-Harshk <harshk1744@gmail.com>
2024-10-08 14:29:17 +0530

    renamed:    index.js -> api/index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const fastify = require('fastify')({ logger: true });
 const helmet = require('@fastify/helmet');  
 const cors = require('@fastify/cors');     
-const subjectsRoutes = require('./routes/subjects');
-const questionsRoutes = require('./routes/questions');
+const subjectsRoutes = require('../routes/subjects');
+const questionsRoutes = require('../routes/questions');
 
 
 fastify.register(helmet); 

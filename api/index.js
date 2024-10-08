48833f407f9c420313ed5ee7640fa3865dffc543
sudo-Harshk <harshk1744@gmail.com>
2024-10-08 14:45:01 +0530

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -4,14 +4,28 @@ const cors = require('@fastify/cors');
 const subjectsRoutes = require('../routes/subjects');
 const questionsRoutes = require('../routes/questions');
 
-
 fastify.register(helmet); 
 fastify.register(cors);   
-
 
 fastify.register(subjectsRoutes);
 fastify.register(questionsRoutes);
 
+fastify.get('/', async (request, reply) => {
+    reply.send({
+        message: 'Welcome to the Skillify API!',
+        endpoints: [
+            {
+                endpoint: '/subjects',
+                description: 'Retrieve information about various subjects'
+            },
+            {
+                endpoint: '/questions',
+                description: 'Retrieve questions and answers for practice'
+            }
+        ],
+        note: 'Use the above endpoints to interact with the API.'
+    });
+});
 
 const start = async () => {
     try {

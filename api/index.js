d25711cb0b0f5576b93687cbeb1570e962f2234e
sudo-Harshk <harshk1744@gmail.com>
2024-10-12 08:06:30 +0530

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -4,15 +4,14 @@ const cors = require('@fastify/cors');
 const subjectsRoutes = require('../routes/subjects');
 const questionsRoutes = require('../routes/questions');
 
-// Register Helmet for basic security best practices
+
 fastify.register(helmet);
 
-// Register CORS with the required configuration
 fastify.register(cors, {
-    origin: ['https://sudo-harshk.github.io'], // Allow requests from your frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers in CORS requests
-    credentials: true // If you need cookies or authentication headers, set this to true
+    origin: ['https://sudo-harshk.github.io'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true 
 });
 
 // Register your routes
@@ -44,7 +43,7 @@ fastify.get('/', async (request, reply) => {
 // Start the server
 const start = async () => {
     try {
-        await fastify.listen({ port: 3000, host: '0.0.0.0' }); // host set to '0.0.0.0' to allow connections from Vercel or other remote locations
+        await fastify.listen({ port: 3000, host: '0.0.0.0' }); 
         fastify.log.info(`Server listening on http://localhost:3000`);
     } catch (err) {
         fastify.log.error(err);
